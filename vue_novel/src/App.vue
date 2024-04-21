b456cb89fad883ce88bd2a4aa1f2646a461a75e8
孙源玲 <3442153944@qq.com>
2024-04-21 09:45:48 +0800

    测试了vue，开始使用vue脚手架来搭建标题栏

---
+++ vue_novel/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="classtest">
+        <h1>classtest</h1>
+        <Person></Person>
+    </div>
+</template>
+<script  lang="ts">
+import Person from './components/Person.vue'
+export default{
+    name:'App',
+    components:{
+        Person
+    }
+}
+
+</script>
+<style>
+
+</style>
