17a48f1c94708d7e7e9cc63114d00b64596c752b
孙源玲 <sunfujiaihaonv@gmail.com>
2024-05-15 11:08:57 +0800

    修改代理服务器和主页的虚拟端口

--- vue_index_page/vite.config.ts
+++ vue_index_page/vite.config.ts
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    port:3002,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:11451', // 代理的目标地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // 如果目标服务器的接口路径有前缀，可以在代理时去掉前缀
+      },
+    },
+  }
 })
