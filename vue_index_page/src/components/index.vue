85c3b6fa063074703bdf99e36fee9e5f13aa62b3
孙源玲 <sunfujiaihaonv@gmail.com>
2024-05-15 11:27:08 +0800

    增加了标题栏，登录跳转更改为首页

---
+++ vue_index_page/src/components/index.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="index">
+    111
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'index',
+
+};
+</script>
+<script setup>
+
+</script>
+
+<style scoped>
+/* 在这里添加组件的样式 */
+</style>
