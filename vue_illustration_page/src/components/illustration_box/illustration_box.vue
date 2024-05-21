74eb8cb6f2bb86be80200ae6818f72873800b975
孙源玲 <sunfujiaihaonv@gmail.com>
2024-05-21 15:02:21 +0800

    查看大图功能实现

--- vue_illustration_page/src/components/illustration_box/illustration_box.vue
+++ vue_illustration_page/src/components/illustration_box/illustration_box.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="illustration_box">
         <div class="ill_image">
-            <img >
+            <div class="ill_image_box" v-for="(path,index) in ill_img_path" :key="index">
+                <img :src="path" @click="show_max_image(path)">
+                
+            </div>
+            <div class="overlay" v-if="max_image_box_is_show" @click="hidden_max_image">
+                <div class="max_image_box" ref="max_image_box" >
+                    <img id="temp_img_box">
+                </div>
+            </div>
         </div>
         <div class="ill_data">
 
@@ -32,7 +40,7 @@
                         <img>
                     </div>
                     <div class="like_svg">
-                        
+
                     </div>
                 </div>
             </div>
@@ -51,7 +59,21 @@ export default{
 </script>
 
 <script setup>
+let ill_img_path=ref(['https://127.0.0.1:4434/image/116883317_p0.png','https://127.0.0.1:4434/image/65014220_p0.jpg']);
+let max_image_box=ref(null);
+let max_image_box_is_show=ref(false);
 
+function show_max_image(path){
+    max_image_box_is_show.value=true;
+    setTimeout(()=>{
+        let temp_box=document.getElementById('temp_img_box');
+        temp_box.src=path;
+    },100)
+        
+}
+function hidden_max_image(){
+    max_image_box_is_show.value=false;
+}
 </script>
 
 <style scoped>
@@ -60,4 +82,47 @@ export default{
         width: 80%;
         height: auto;
     }
+    .ill_image{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: auto;
+        overflow: hidden;
+    }
+    .ill_image img{
+        width: 100%;
+        height: auto;
+        object-fit: cover;
+    }
+    .ill_image_box{
+        width:100%;
+        height: auto;
+        overflow: hidden;
+        margin-top:10px;
+    }
+    .ill_image_box:hover{
+        cursor:pointer;
+    }
+    .max_image_box{
+        width: auto;
+        height: auto;
+        z-index: 10;
+        background-color: rgba(0,0,0,1);
+        position: absolute;
+        top:0px;
+        left:0px;
+        object-fit: cover;
+        cursor: pointer;
+    }
+    .overlay{
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,0.5);
+        position: fixed;
+        top:0px;
+        left:0px;
+        z-index: 10;
+        overflow: auto;
+    }
+    
 </style>
