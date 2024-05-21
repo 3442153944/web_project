def820f645a07c0336832e73aa0ad8f959188b99
孙源玲 <sunfujiaihaonv@gmail.com>
2024-05-21 09:32:23 +0800

    排行榜组件完善

--- vue_index_page/src/components/novel_page/ranking.vue
+++ vue_index_page/src/components/novel_page/ranking.vue
@@ -1,10 +1,47 @@
 <template>
   <div class="ranking">
     <h5 style="margin-bottom:0px;">每日排行榜</h5>
-    <h3 style="margin-top:5px;">{{dateStr}}的排行榜</h3>
+    <h3 style="margin-top:5px;">{{ dateStr }}的排行榜</h3>
     <div class="ranking_work_list">
-        <div class="work_item"></div>
+      <div class="work_item" v-for="index in 5" :key="index">
+        <div class="cover">
+          <div class="rank_svg">
+            1
+          </div>
+          <img :src="cover_path">
+        </div>
+        <div class="work_info">
+          <div class="work_title">
+            <span>{{work_title}}</span>
+          </div>
+          <div class="work_chapter">
+            <span>{{work_chapter}}</span>
+          </div>
+          <div class="user_info">
+            <div class="user_avatar">
+              <img :src="user_avatar">
+            </div>
+            <div class="username">
+              <span>{{username}}</span>
+            </div>
+          </div>
+          <div class="work_tags">
+            <span v-for="(tag,index) in work_tags" :key="index" style="margin-right: 5px;">#{{tag}}</span>
+          </div>
+          <div class="brief_introduction">
+            <span>{{brief_introduction}}</span>
+          </div>
+          <div class="read_data">
+            <span>{{read_data.word}}字</span>
+            <span>{{read_data.read_time}}分钟</span>
+            <span>{{read_data.read_num}}次</span>
+          </div>
+        </div>
+      </div>
+      
     </div>
+    <div class="left_btn">1</div>
+      <div class="right_btn">1</div>
   </div>
 </template>
 
@@ -18,19 +55,178 @@ export default {
 </script>
 
 <script setup>
-let date=new Date();
-let year=date.getFullYear();
-let month=date.getMonth()+1;
-let day=date.getDate();
-let dateStr=year+'-'+month+'-'+day;
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let dateStr = year + '-' + month + '-' + day;
+
+let cover_path=ref('https://127.0.0.1:4434/image/118839253_p1.jpg');
+let rank=ref(1);
+let work_title=ref('作品标题');
+let work_chapter=ref('章节标题');
+let user_avatar=ref('https://127.0.0.1:4434/image/116883317_p0.png');
+let username=ref('用户名');
+let work_tags=ref(['标签1','标签2','标签3','标签1','标签2','标签3','标签1','标签2','标签3']);
+let brief_introduction=ref('作品简介');
+let read_data=ref({
+  word:1000,
+  read_time:10,
+  read_num:1000
+});
 </script>
 
 <style scoped>
-  .ranking{
-    display: flex;
-    width:100%;
-    height: auto;
-    flex-direction: column;
-    margin-top:20px;
-  }
+.ranking {
+  display: flex;
+  width: 100%;
+  height: auto;
+  flex-direction: column;
+  margin-top: 20px;
+  position: relative;
+}
+
+.ranking_work_list {
+  display: flex;
+  width: 100%;
+  height: auto;
+  min-height: 350px;
+  margin-top: 10px;
+  overflow-x: auto;
+  scrollbar-width: none;
+  white-space: nowrap;
+  position: relative;
+}
+
+.work_item {
+  display: flex;
+  width: calc(100%/2);
+  min-width: 450px;
+  height: auto;
+  min-height: 350px;
+  position: relative;
+  border-radius: 15px;
+  overflow: hidden;
+  margin-right: 40px;
+}
+
+.cover {
+  display: flex;
+  width: 30%;
+  height: 100%;
+  position: relative;
+  border-radius: 15px;
+  overflow: hidden;
+}
+
+.cover img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.rank_svg {
+  position: absolute;
+  left: 5px;
+  top: 5px;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(236, 87, 41, 1);
+}
+
+.work_info {
+  display: flex;
+  width: 70%;
+  height: 100%;
+  flex-direction: column;
+  padding: 10px;
+  flex-grow: 1;
+  justify-content: space-around;
+
+}
+
+.work_title {
+  display: flex;
+  width: 100%;
+  height: auto;
+  font-size: 20px;
+  font-weight: bold;
+  color: rgba(123, 123, 123, 1);
+}
+
+.work_chapter {
+  width: 100%;
+  height: auto;
+  font-size: 18px;
+  font-weight: bold;
+}
+.work_tags{
+  font-size: 16px;
+  font-weight: bold;
+  color:rgba(0,150,250,1);
+  white-space: break-spaces;
+}
+
+.user_info {
+  display: flex;
+  width: 100%;
+  height: auto;
+}
+
+.user_avatar {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.user_avatar img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.username {
+  margin-left: 10px;
+}
+.read_data{
+  display: flex;
+  justify-content: space-between;
+}
+.left_btn{
+  position: absolute;
+  width: 50px;
+  height: 350px;
+  left:0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background:linear-gradient(to right,rgba(255,255,255,1),rgba(255,255,255,0));
+  top:98px;
+  opacity: 0;
+  z-index: 2;
+}
+.left_btn:hover,.right_btn:hover{
+  cursor: pointer;
+  opacity: 1;
+  transition:all 0.3s ease-in-out;
+  z-index: 2;
+}
+.right_btn{
+  position: absolute;
+  width:50px;
+  height: 350px;
+  right: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background:linear-gradient(to left,rgba(255,255,255,1),rgba(255,255,255,0));
+  top:98px;
+  opacity: 0;
+  z-index: 2;
+}
 </style>
