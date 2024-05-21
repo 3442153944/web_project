3d98cfe41ec96899051b22c3da65275703b37282
孙源玲 <sunfujiaihaonv@gmail.com>
2024-05-21 10:18:04 +0800

    新增插画详情页面，更新启动脚本

---
+++ vue_illustration_page/src/components/index.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="index">
+    主页面
+</div>
+</template>
+<script>
+import {ref,onMounted,onUnmounted,watch} from 'vue';
+export default{
+    name:'index',
+    components:{},
+}
+</script>
+<script setup>
+
+</script>
+<style scoped>
+.index{
+    display: flex;
+    width:80%;
+    height: auto;
+    margin:20px auto;
+}
+</style>
