3cad52241d3d0b884b9d94df50f703be75aeafe7
孙源玲 <3442153944@qq.com>
2024-05-23 16:11:15 +0800

    新增用户中心页面，对标题栏进行了的文件结构进行了优化方便后续引用

---
+++ vue_usercenter_page/src/components/rootpage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="rootpage">
+    <header_box></header_box>
+    主vue页面
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import { ref, reactive, toRefs, watch, onMounted, onUnmounted } from 'vue';
+import header_box from './headpage_file/header_box.vue';
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'rootpage',
+  components:{header_box},
+}
+</script>
+
+<script setup>
+</script>
+
+<style scoped>
+  
+</style>
