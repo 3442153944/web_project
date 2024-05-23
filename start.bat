dbbbf7a2750ec57ad130134f9a3f082c597fb485
孙源玲 <sunfujiaihaonv@gmail.com>
2024-05-23 09:25:28 +0800

    新建了一个用户中心项目，更改了配置文件，更新了启动脚本

--- start.bat
+++ start.bat
@@ -7,5 +7,6 @@ start "" "cmd" /k "cd /d H:\web_preject\vue_login && npm run dev"
 start "" "cmd" /k "cd /d H:\web_preject\vue_index_page && npm run dev"
 
 start "" "cmd" /k "cd /d H:\web_preject\vue_illustration_page && npm run dev"
+start "" "cmd" /k "cd /d H:\web_preject\vue_usercenter_page && npm run dev"
   
 pause
