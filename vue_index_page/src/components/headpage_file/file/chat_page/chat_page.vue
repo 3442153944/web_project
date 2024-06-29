cc177627ab4e9ab864278baeff0c16eee28cc937
孙源玲 <3442153944@qq.com>
2024-06-29 13:23:49 +0800

    对聊天页面进行了完善

---
+++ vue_index_page/src/components/headpage_file/file/chat_page/chat_page.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="chat_page">
+    <div class="close_btn" @click="close_page_click"></div>
+    聊天页面
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs, watch, onMounted, onUnmounted,defineEmits } from 'vue';
+export default {
+  name: 'chat_page',
+}
+</script>
+
+<script setup>
+let close_page=defineEmits(['close_page']);
+//向父组件传递关闭消息
+function close_page_click(){
+  close_page('close_page',false)
+}
+
+</script>
+
+<style scoped>
+  
+</style>
