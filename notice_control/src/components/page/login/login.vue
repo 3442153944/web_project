839e07ea8c667c8ba4ad2f825a481ba6ed0820c8
孙源玲 <3442153944@qq.com>
2024-07-14 03:08:55 +0800

    几经周折，终于完成了一个新的登录认证方式，动态UUID认证获得了更高的安全性

---
+++ notice_control/src/components/page/login/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login">
+    登录页面
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs, watch, onMounted, onUnmounted } from 'vue';
+export default {
+  name: 'login',
+}
+</script>
+
+<script setup>
+</script>
+
+<style scoped>
+  
+</style>
