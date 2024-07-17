4431be35120e756da228fae5d611d692969e6b13
孙源玲 <3442153944@qq.com>
2024-07-17 12:07:05 +0800

    .

--- vue_index_page/src/components/headpage_file/file/contribute/index.vue
+++ vue_index_page/src/components/headpage_file/file/contribute/index.vue
@@ -1,61 +1,105 @@
 <template>
   <div class="index">
-    <div class="close_btn" @click="close_upload_page()">
+    <div class="choose_page">
+      <div class="close_btn" @click="close_upload_page()">
         <img class="icon" src="https://www.sunyuanling.com/assets/close.svg">
+      </div>
+      <span @click="type = 'ill'" class="choose_page_item">插画</span>
+      <span @click="type = 'comic'" class="choose_page_item">漫画</span>
+      <span @click="type = 'novel'" class="choose_page_item">小说</span>
     </div>
-    <ill_page v-if="type=='ill'"></ill_page>
-    <comic_page v-else-if="type=='comic'"></comic_page>
-    <novel_page v-else-if="type=='novel'"></novel_page>
+    <ill_page v-if="type == 'ill'"></ill_page>
+    <comic_page v-else-if="type == 'comic'"></comic_page>
+    <novel_page v-else-if="type == 'novel'"></novel_page>
   </div>
 </template>
 
 <script>
 // eslint-disable-next-line no-unused-vars
-import { ref, reactive, toRefs, watch, onMounted, onUnmounted,defineEmits } from 'vue';
+import { ref, reactive, toRefs, watch, onMounted, onUnmounted, defineEmits } from 'vue';
 import ill_page from './ill/ill_page.vue';
 import comic_page from './comic/comic_page.vue';
 import novel_page from './novel/novel_page.vue';
-import{useStore } from 'vuex'
+import { useStore } from 'vuex'
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'index',
-  components:{
-    ill_page,comic_page,novel_page
+  components: {
+    ill_page, comic_page, novel_page
   }
 }
 </script>
 
 <script setup>
-const store=useStore()
-let type=ref();
-function close_upload_page()
-{
-    store.commit('SET_SINGLE_PAGE_STATUS',{key:'upload_work',value:false})
+const store = useStore()
+let type = ref();
+function close_upload_page() {
+  store.commit('SET_SINGLE_PAGE_STATUS', { key: 'upload_work', value: false })
 }
-watch(()=>store.getters.upload_work_type,(newValue)=>{
-    type.value=newValue;
-    console.log(newValue)
+watch(() => store.getters.upload_work_type, (newValue) => {
+  type.value = newValue;
+  console.log(newValue)
 })
-onMounted(()=>{
-    type.value=store.getters.upload_work_type;
+onMounted(() => {
+  type.value = store.getters.upload_work_type;
 })
 </script>
 
 <style scoped>
-  .icon{
-    width: 25px;
-    height: 25px;
-    object-fit: cover;
-  }
-  .index{
-    width: 100vw;
-    min-height: 90vh;
-    height: 100%;
-    display: flex;
-    z-index: 15;
-    background-color: rgba(255, 255, 255, 1);
-    position: fixed;
-    top: 65px;
-    left: 0px;
-  }
+.icon {
+  width: 25px;
+  height: 25px;
+  object-fit: cover;
+}
+
+.index {
+  width: 100vw;
+  min-height: 90vh;
+  height: 100%;
+  display: flex;
+  background-color: rgba(255, 255, 255, 1);
+  top: 65px;
+  left: 0px;
+  flex-direction: column
+}
+
+.choose_page {
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(77, 77, 77, 1);
+  margin: 5px auto;
+  position: relative;
+  color:rgba(233,233,233,1);
+  font-size: 16px;
+  font-weight: bold;
+}
+.close_btn{
+  width:30px;
+  height: 30px;
+  position: absolute;
+  left: 10px;
+  margin:auto;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+.close_btn:hover{
+  cursor: pointer;
+  background-color:rgba(188,188,188,1);
+  border-radius: 50%;
+  transition:all 0.3s ease-in-out;
+}
+.choose_page_item {
+  width: auto;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px 10px;
+  padding: 2px 10px;
+  cursor: pointer;
+}
 </style>
