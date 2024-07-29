78168d8df55f0bd7060b8030a2a2fd5634ebf2d7
孙源玲 <3442153944@qq.com>
2024-07-29 17:38:07 +0800

    删除了大量的重复无关代码，对内容进行了补全，优化了页面结构，重构了大量的屎山

---
+++ model/components/virtual_scroll_box/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import virtual_scroll_box from './components/virtual_scroll_index.vue'
+</script>
+
+<template>
+ 
+  <virtual_scroll_box  />
+</template>
+
+<style scoped>
+
+</style>
---
+++ model/components/virtual_scroll_box/src/components/virtual_scroll_index.vue
@@ -0,0 +1,23 @@
+<!-- src/components/VirtualScrollBox.vue -->
+<script setup>
+import { ref, computed } from 'vue'
+import InfiniteScroll from './virtual_scroll_box.vue'
+
+
+
+</script>
+
+<template>
+  <InfiniteScroll >
+  
+  </InfiniteScroll>
+</template>
+
+<style scoped>
+.item {
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
