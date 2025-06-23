8c5788f65cc641ba024bafaab08f287f88854946
sunyuanling <sunfujiaihaonv@gmail.com>
2025-06-23 22:14:01 +0800

    细节修改

---
+++ web/src/components/content_page/novel_page/novel_components/model/novel_content_model/model/chapter_content.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="chapter_content">
+        
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import {useRouter} from 'vue-router'
+
+// 响应式数据
+const data = ref(null)
+const router = useRouter()
+</script>
+
+<style scoped lang="scss">
+/* 样式代码 */
+</style>
